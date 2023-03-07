aa9478a232ed6a44729f3eb65efd6b50abffb922
RadonCoding <86915746+RadonCoding@users.noreply.github.com>
2023-03-08 01:45:37 +0200

    Update OverlayByakuganView.java

--- src/main/java/net/narutomod/gui/overlay/OverlayByakuganView.java
+++ src/main/java/net/narutomod/gui/overlay/OverlayByakuganView.java
@@ -182,7 +182,7 @@ public class OverlayByakuganView extends ElementsNarutomodMod.ModElement {
 			if ((event.getButton() == 0 || event.getButton() == 1) && event.isButtonstate()) {
 				Minecraft mc = Minecraft.getMinecraft();
 
-				if (mc.objectMouseOver.entityHit instanceof EntityPlayer && mc.objectMouseOver.entityHit.equals(mc.player)) {
+				if (mc.objectMouseOver != null && mc.objectMouseOver.entityHit instanceof EntityPlayer && mc.objectMouseOver.entityHit.equals(mc.player)) {
 					event.setCanceled(true);
 				}
 			}
