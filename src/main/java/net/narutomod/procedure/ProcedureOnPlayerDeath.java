f0c1a3cb0fbd5b8e42514dff180cd50b938df42c
RadonCoding <henry.welch@reborn.com>
2023-03-12 22:53:37 +0200

    See ChangeLog.txt

--- src/main/java/net/narutomod/procedure/ProcedureOnPlayerDeath.java
+++ src/main/java/net/narutomod/procedure/ProcedureOnPlayerDeath.java
@@ -210,9 +210,30 @@ public class ProcedureOnPlayerDeath extends ElementsNarutomodMod.ModElement {
 						((stack)).shrink((int) 1);
 					}
 					if (((entity instanceof EntityPlayer)
-							? ((EntityPlayer) entity).inventory.hasItemStack(new ItemStack(ItemByakugan.helmet, (int) (1)))
+							? ((EntityPlayer) entity).inventory.hasItemStack(new ItemStack(ItemMangekyoSharinganObito.helmet, (int) (1)))
 							: false)) {
 						stack = ProcedureUtils.getMatchingItemStack((EntityPlayer) entity, ItemByakugan.helmet);
+						((EntityPlayer) entity).dropItem(stack.copy(), true, true);
+						((stack)).shrink((int) 1);
+					}
+					if (((entity instanceof EntityPlayer)
+							? ((EntityPlayer) entity).inventory.hasItemStack(new ItemStack(ItemMangekyoSharinganEternal.helmet, (int) (1)))
+							: false)) {
+						stack = ProcedureUtils.getMatchingItemStack((EntityPlayer) entity, ItemMangekyoSharinganEternal.helmet);
+						((EntityPlayer) entity).dropItem(stack.copy(), true, true);
+						((stack)).shrink((int) 1);
+					}
+					if (((entity instanceof EntityPlayer)
+							? ((EntityPlayer) entity).inventory.hasItemStack(new ItemStack(ItemRinnegan.helmet, (int) (1)))
+							: false)) {
+						stack = ProcedureUtils.getMatchingItemStack((EntityPlayer) entity, ItemRinnegan.helmet);
+						((EntityPlayer) entity).dropItem(stack.copy(), true, true);
+						((stack)).shrink((int) 1);
+					}
+					if (((entity instanceof EntityPlayer)
+							? ((EntityPlayer) entity).inventory.hasItemStack(new ItemStack(ItemTenseigan.helmet, (int) (1)))
+							: false)) {
+						stack = ProcedureUtils.getMatchingItemStack((EntityPlayer) entity, ItemTenseigan.helmet);
 						((EntityPlayer) entity).dropItem(stack.copy(), true, true);
 						((stack)).shrink((int) 1);
 					}
